fb0700def943e4361250db678ea2ab35548cd06c
Ko3ch <tomko3ch@gmail.com>
2020-07-16 17:56:01 +0300

    migrated db

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     config class to declare app's main configurations
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tom:psql3848@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tom:psql3848@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
@@ -26,15 +26,11 @@ class Config:
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tom:psql3848@localhost/pitch_test'
-
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tom:psql3848@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tom:psql3848@localhost/pitches'
     DEBUG = True
 
 config_options = {
 'development':DevConfig,
 'production':ProdConfig,
-'test':TestConfig
 }
