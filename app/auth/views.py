afd904c9f2a0b37c8cd9c783989d4083125f6788
Ko3ch <tomko3ch@gmail.com>
2020-07-16 19:56:28 +0300

    edited

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,7 +13,7 @@ def login():
         user = Users.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            return redirect(request.args.get('next') or url_for('main.pitches'))
 
         flash('Invalid username or Password')
 
