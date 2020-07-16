fb0dba499f4eb13dba544899d4509f469ca36fae
Ko3ch <tomko3ch@gmail.com>
2020-07-16 15:16:54 +0300

    migrations

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,8 @@ def create_app(config_name):
     app = Flask(__name__)
 
     app.config.from_object(config_options[config_name])
-    
+    config_options[config_name].init_app(app)
+
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
@@ -37,3 +38,4 @@ def create_app(config_name):
     configure_uploads(app,photos)
 
     return app
+
