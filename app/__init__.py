8d2727a9f986c456ef7548bc4bcf178e40425ee1
Ko3ch <tomko3ch@gmail.com>
2020-07-16 14:18:46 +0300

    edited

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,6 @@ photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
     app = Flask(__name__)
 
-    app.config.from_object(config_options[config_name])
     config_options[config_name].init_app(app)
 
     bootstrap.init_app(app)
